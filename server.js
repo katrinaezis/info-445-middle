9dfad3a9f4f375ace00be1cfe8510d14bb6192c7
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-05-31 15:59:00 -0700

    Adds comment deletion

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var sql = require('mssql')
 var app = require('express')()
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 function getPass () {
   var pass = process.env.PASSWORD
@@ -27,6 +28,12 @@ function getArticles () {
   return new sql.Request().query('SELECT TOP 20 * FROM ARTICLE')
 }
 
+// Returns the comments for a given article ID
+function getComments (id) {
+  const q = `SELECT * FROM COMMENT WHERE ArticleID = ${id}`
+  return new sql.Request().query(q)
+}
+
 function addComment (fName, lName, articleTitle, content) {
   return new sql.Request()
     .input('personFName', fName)
@@ -36,9 +43,34 @@ function addComment (fName, lName, articleTitle, content) {
     .execute('uspAddComment')
 }
 
+// Deletes the comment with a given ID
+function deleteComment (id) {
+  const delComment = () => {
+    const commentDeleteQ = `DELETE FROM COMMENT WHERE CommentID = ${id}`
+    return new sql.Request().query(commentDeleteQ)
+  }
+
+  const delCommentReview = () => {
+    const commentReviewDeleteQ = `DELETE FROM REVIEW_COMMENT WHERE CommentID = ${id}` 
+    return new sql.Request().query(commentReviewDeleteQ)
+  }
+
+  var tran = new sql.Transaction()
+  return tran.begin()
+    .then(delComment())
+    .then(delCommentReview())
+    .then(() => {
+      // We only get here if the above deletions succeed
+      return tran.commit()
+    })
+}
+
+function loadMiddleWare () {
+  app.use(cors()) // So we can use different hosts
+  app.use(bodyParse.json()) // For handling post requests
+}
+
 function makeRouter () {
-  app.use(cors())
-  
   app.get('/articles', (req, res) => {
     getArticles().then((data) => {
       res.json(data)
@@ -46,16 +78,31 @@ function makeRouter () {
   })
 
   // Makes a test comment and writes it out to the database!
-  app.post('/articles', (req, res) => {
+  app.post('/comments', (req, res) => {
     addComment(req.body.lname, req.body.lname, req.body.title, req.body.content)
       .then(() => { console.log('Added new comment')} )
+  })
+
+  // Get's all the comments associated with a given articleID
+  app.get('/comments/:id', (req, res) => {
+    getComments(req.params.id)
+      .then(() => { res.sendStatus(200) })
+      .catch((e) => { res.status(400).json({ error: e }) })
+  })
+
+  // Deletes the comment from the database
+  app.delete('/comments/:id', (req, res) => {
+    deleteComment(req.params.id)
+      .then(() =>{ res.sendstatus(200) })
+      .catch((e) => { res.status(400).json({ error: e }) })
   })
 }
 
 function startParty () {
   connectToDb().then(() => {
+    loadMiddleWare()
     makeRouter()
-    app.listen(3000);
+    app.listen(3000)
   })
 }
 
